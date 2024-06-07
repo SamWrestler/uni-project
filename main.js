aef099a5419bb5491c8bf767a007ecefb1533285
Saman Mokhtari <saman.mokhtari99@gmail.com>
2024-06-07 10:49:36 +0330

    a little dashboard

--- main.js
+++ main.js
@@ -46,3 +46,43 @@ var swiper2 = new Swiper(".mySwiper2", {
     prevEl: '.swiper-button-prev',
   },
 })
+
+const button = document.querySelectorAll('.collapse-btn')
+button.forEach(element => {
+  element.addEventListener('click', (e) => {
+    if (element.nextElementSibling.classList.contains(['max-h-[0]'])){
+      element.lastElementChild.innerHTML = `<i class="fa-light fa-minus"></i>`
+    }else{
+      element.lastElementChild.innerHTML = `<i class="fa-light fa-angle-down text-[1.2rem]"></i>`
+    }
+    element.nextElementSibling.classList.toggle('max-h-[0]')
+    element.nextElementSibling.classList.toggle('mt-4')
+    
+  })
+});
+
+
+const profile_panel = document.querySelector('.profile-panel')
+const journal_panel = document.querySelector('.journal-panel')
+document.querySelector('.profile').addEventListener('click', (e) => {
+  gsap.to('.profile-panel', { left: "10%", duration:0.2, direction:"rtl"})
+  profile_panel.classList.add('side-panel-shadow')
+})
+
+document.querySelector('.close-profile').addEventListener('click', (e) => {
+  gsap.to('.profile-panel', { left: "100%", duration:0.2, direction:"rtl"})
+  profile_panel.classList.remove('side-panel-shadow')
+
+})
+
+document.querySelector('.journal').addEventListener('click', (e) => {
+  gsap.to('.journal-panel', { left: "10%", duration:0.2, direction:"rtl"})
+  profile_panel.classList.add('side-panel-shadow')
+
+})
+
+document.querySelector('.close-journal').addEventListener('click', (e) => {
+  gsap.to('.journal-panel', { left: "100%", duration:0.2, direction:"rtl"})
+  profile_panel.classList.remove('side-panel-shadow')
+
+})
